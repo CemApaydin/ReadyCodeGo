d442a06674b80e5081cbc6b3ee20c762637804ff
Fecth <76708517+Fecth@users.noreply.github.com>
2022-12-13 22:45:23 +0300

    jpaCorrection
    jpaRepo Corrected

--- backEnd/src/main/java/com/example/backEnd/facultyboardmember/FacultyBoardMemberRepository.java
+++ backEnd/src/main/java/com/example/backEnd/facultyboardmember/FacultyBoardMemberRepository.java
@@ -1,7 +1,9 @@
 package com.example.backEnd.facultyboardmember;
+import com.example.backEnd.coordinator.Coordinator;
 import com.example.backEnd.user.UserRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FacultyBoardMemberRepository extends UserRepository<FacultyBoardMember> {
+public interface FacultyBoardMemberRepository extends JpaRepository<FacultyBoardMember, Long> {
 }
--- backEnd/src/main/java/com/example/backEnd/user/UserRepository.java
+++ backEnd/src/main/java/com/example/backEnd/user/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.stereotype.Repository;
 
+
 @NoRepositoryBean
-public interface UserRepository<T extends User> extends JpaRepository<T, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
